973650ad54c3ded3f869db0d63975f5a3ef7403d
Azizullah <azizullahcodes@gmail.com>
2025-08-11 06:51:39 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function palindroming(){let getWord = document.getElementById('input').value;
+    let display = document.getElementById('display');
+    let reversed = '';
+    for(let i = getWord.length-1; i >=0;i--){reversed = reversed + getWord[i];
+        if(reversed === getWord){
+        display.innerHTML = `${getWord} is palindrome`
+    }
+    else{
+       display.innerHTML = `${getWord} is not palindrome`
+    }
+    };
+    
+    
+}
