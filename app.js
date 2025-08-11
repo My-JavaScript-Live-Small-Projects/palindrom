8d5f6343bc875123aff64d312558a13cc096ba4a
Azizullah <azizullahcodes@gmail.com>
2025-08-11 07:04:57 -0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function palindroming(){let getWord = document.getElementById('input').value;
+    getWord = getWord.toLowerCase();
     let display = document.getElementById('display');
     let reversed = '';
     for(let i = getWord.length-1; i >=0;i--){reversed = reversed + getWord[i];
